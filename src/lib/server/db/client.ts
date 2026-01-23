a21a3221a2bbda29fa609c344b77ba6cdc832f13
Oscar "osbyrne" Byrne <osbyrne1@gmail.com>
2026-01-23 13:05:23 +0100

    it works now

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,10 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
+import { config } from 'dotenv';
+
+config({ path: '.env' });
 
 const turso = createClient({
-  url: TURSO_DATABASE_URL,
-  authToken: TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(turso);
