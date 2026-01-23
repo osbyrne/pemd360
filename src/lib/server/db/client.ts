40c787933ea5e1de79ce7ec77d6358f3a4235c5f
Oscar "osbyrne" Byrne <osbyrne1@gmail.com>
2026-01-23 20:12:58 +0100

    changed.ts

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,4 +1,12 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { DATABASE_URL } from '$env/static/private';
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import { config } from 'dotenv';
 
-export const db = drizzle({ connection: { source: DATABASE_URL } });
+config({ path: '.env' });
+
+const turso = createClient({
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
+export const db = drizzle(turso);
