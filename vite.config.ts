7d7706ff5a3ddcc6decb8db544c00b67e83740f1
Oscar "osbyrne" Byrne <osbyrne1@gmail.com>
2026-03-12 09:56:15 +0100

    format

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,33 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				id: 'pemd360',
-				name: 'PEMD360',
-				short_name: 'PEMD360',
-				description: 'La Web-Application PEMD360 par Dépollution Conseil',
-				display: 'minimal-ui',
-				orientation: 'landscape',
-				start_url: '/app',
-				background_color: '#ffffff',
-				theme_color: '#009966',
-				dir: 'ltr',
-				lang: 'fr',
-				categories: ['utilities', 'business'],
-				icons: [{ src: 'favicon.png', sizes: '429x429', type: 'image/png' }]
-			},
-			devOptions: { enabled: true }
-		}),
-		devtoolsJson()
-	]
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        SvelteKitPWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                id: 'pemd360',
+                name: 'PEMD360',
+                short_name: 'PEMD360',
+                description: 'La Web-Application PEMD360 par Dépollution Conseil',
+                display: 'minimal-ui',
+                orientation: 'landscape',
+                start_url: '/app',
+                background_color: '#ffffff',
+                theme_color: '#009966',
+                dir: 'ltr',
+                lang: 'fr',
+                categories: ['utilities', 'business'],
+                icons: [{ src: 'favicon.png', sizes: '429x429', type: 'image/png' }]
+            },
+            devOptions: { enabled: true }
+        }),
+        devtoolsJson()
+    ],
+    resolve: {
+        alias: {
+            Matterport_SDK: `./src/lib/sdk.es6.js?applicationKey=${process.env.YOUR_SDK_KEY_HERE}`
+        }
+    }
 });
